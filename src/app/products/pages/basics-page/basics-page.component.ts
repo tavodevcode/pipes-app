26192f87ced229d084e51df6c5d77df36dbddbe0
tavodevcode <tavodevcode@gmail.com>
2024-01-15 23:53:47 -0600

    📝 Add basics-page component with pipes

---
+++ src/app/products/pages/basics-page/basics-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-basics-page',
+  templateUrl: './basics-page.component.html',
+  styleUrls: ['./basics-page.component.css'],
+})
+export class BasicsPageComponent {
+  public nameLower: string = 'gustavo';
+  public nameUpper: string = 'GUSTAVO';
+  public fullName: string = 'gustavo HERNANDEZ';
+}
