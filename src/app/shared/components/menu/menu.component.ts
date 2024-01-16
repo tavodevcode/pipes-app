31d86c48fa0d78f5becd99864f6b6003b00ee649
tavodevcode <tavodevcode@gmail.com>
2024-01-15 23:53:11 -0600

    📝 Add menu component with initial implementation

---
+++ src/app/shared/components/menu/menu.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'shared-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css'],
+})
+export class MenuComponent implements OnInit {
+  public menuItems: MenuItem[] = [];
+
+  ngOnInit() {
+    this.menuItems = [
+      {
+        label: 'Pipes de angular',
+        icon: 'pi pi-desktop',
+        items: [
+          {
+            label: 'Textos y fechas',
+            icon: 'pi pi-align-left',
+            routerLink: '/',
+          },
+          { label: 'Numeros', icon: 'pi pi-dollar', routerLink: 'numbers' },
+          { label: 'No comunes', icon: 'pi pi-globe', routerLink: 'uncommon' },
+        ],
+      },
+      {
+        label: 'Pipes personalizados',
+        icon: 'pi pi-cog',
+        items: [{ label: 'Otros elementos', icon: 'pi pi-cog' }],
+      },
+    ];
+  }
+}
