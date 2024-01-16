73757162cc2bfd6618c1c9bd8bbfe373dcf8949f
tavodevcode <tavodevcode@gmail.com>
2024-01-15 23:54:01 -0600

    📝 Add products routing and module

---
+++ src/app/products/products.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BasicsPageComponent } from './pages/basics-page/basics-page.component';
+import { NumbersPageComponent } from './pages/numbers-page/numbers-page.component';
+import { UncommonPageComponent } from './pages/uncommon-page/uncommon-page.component';
+import { ProductsRoutingModule } from './products-routing.module';
+import { PrimeNgModule } from '../prime-ng/prime-ng.module';
+
+@NgModule({
+  declarations: [
+    BasicsPageComponent,
+    NumbersPageComponent,
+    UncommonPageComponent,
+  ],
+  imports: [CommonModule, ProductsRoutingModule, PrimeNgModule],
+})
+export class ProductsModule {}
