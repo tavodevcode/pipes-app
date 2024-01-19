86d0be9b13246c36204c7c25cc98a039faad036f
tavodevcode <tavodevcode@gmail.com>
2024-01-18 22:56:39 -0600

    📝 Add totalSells and percent properties to NumbersPageComponent

--- src/app/products/pages/numbers-page/numbers-page.component.ts
+++ src/app/products/pages/numbers-page/numbers-page.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-numbers-page',
   templateUrl: './numbers-page.component.html',
-  styleUrls: ['./numbers-page.component.css']
+  styleUrls: ['./numbers-page.component.css'],
 })
 export class NumbersPageComponent {
-
+  public totalSells: number = 12121789.5567;
+  public percent: number = 0.12987123;
 }
