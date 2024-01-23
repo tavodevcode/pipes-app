c83f061fb99aedb4ab78970e44b7fb2af9e05f16
tavodevcode <tavodevcode@gmail.com>
2024-01-22 23:51:49 -0600

    📝 Add OrderComponent and pipes to ProductsModule

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -5,12 +5,22 @@ import { NumbersPageComponent } from './pages/numbers-page/numbers-page.componen
 import { UncommonPageComponent } from './pages/uncommon-page/uncommon-page.component';
 import { ProductsRoutingModule } from './products-routing.module';
 import { PrimeNgModule } from '../prime-ng/prime-ng.module';
+import { OrderComponent } from './pages/order/order.component';
+import { ToggleCasePipe } from './pipes/toggle-case.pipe';
+import { CanFlyPipe } from './pipes/can-fly.pipe';
+import { SortByPipe } from './pipes/sort-by.pipe';
 
 @NgModule({
   declarations: [
     BasicsPageComponent,
     NumbersPageComponent,
     UncommonPageComponent,
+    OrderComponent,
+
+    // PIPES
+    ToggleCasePipe,
+    CanFlyPipe,
+    SortByPipe,
   ],
   imports: [CommonModule, ProductsRoutingModule, PrimeNgModule],
 })
